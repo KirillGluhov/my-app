9a4298746e1137d2c6a7615aaf0c834093281dac
VladislavSeletskiy <azazalalka1338@gmail.com>
2024-02-18 00:16:33 +0700

    question

--- KeyTracingAPI/KeyTracingAPI/Models/Enums/KeyStatus.cs
+++ KeyTracingAPI/KeyTracingAPI/Models/Enums/KeyStatus.cs
@@ -6,7 +6,8 @@ namespace KeyTracingAPI.Models.Enums
     public enum KeyStatus
     {
         Free = 1,
-        InProcess = 2,
+        InProcess = 2, //zachem status y klyhcha? status uze est u zaiavki, a klych libo lezit v kabinete dekanata libo net. daze esli para konchilas, klych prinadlezit predidushemi uchastniku, poka ne peredal sleduushemu.
         Booked = 3
+            //edinstvenniy variant vizu kak klych poterian, vse, no v takom sluchae on toze ne v kabite dekanata (a daze esli tam to ob etom ne znayut)
     }
 }
