93ed2a24157917f9428af0637e3c37af97b393f1
Kirill Glukhov <valeralunin34@gmail.com>
2024-02-29 01:41:05 +0700

    Рабочий вариант (но с костылями)

--- src/components/UsersPage/UserCard.js
+++ src/components/UsersPage/UserCard.js
@@ -1,19 +1,18 @@
 import { Form, Row, Col, Stack, Button } from 'react-bootstrap';
 import "../../styles/requestcard.css";
-import { Box } from '@mui/joy';
 
-function UserCard({ user }) {
+function UserCard(user) {
     return (
         <Stack className='border-darkblue min-270' style={{width: '60%' }}>
             <Row className='mt-3 mx-1'>
                 <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={4} className='mb-3 p-6 border-radius-small-all'>
-                    <Form.Control className='radiusnone center' plaintext readOnly defaultValue="Студент"/>
+                    <Form.Control className='radiusnone center' plaintext readOnly defaultValue={user.role}/>
                 </Col>
                 <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={4} className='mb-3 p-6'>
-                    <Form.Control className='radiusnone center' plaintext readOnly defaultValue="yakrolik@mail.ru" />
+                    <Form.Control className='radiusnone center' plaintext readOnly defaultValue={user.email} />
                 </Col>
                 <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={4} className='mb-3 p-6'>
-                    <Form.Control className='radiusnone center' plaintext readOnly value={ user.name } />
+                    <Form.Control className='radiusnone center' plaintext readOnly defaultValue={ user.name } />
                 </Col>
             </Row>
         </Stack>
--- src/components/UsersPage/Users.js
+++ src/components/UsersPage/Users.js
@@ -5,65 +5,44 @@ import UsersFilter from './UsersFilter';
 import UserCard from './UserCard';
 import { useEffect, useState } from 'react';
 
-function Users(props)
-{
-    const [loading, setLoading] = useState(true);
-    const [users, setUsers] = useState();
+const AsyncComponent = () => {
+  const [users, setData] = useState(null);
 
-    async function fetchUsers1() {
-        try {
-            setLoading(true);
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
-            const users = await response.json();
-            setUsers(users);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            setLoading(false);
-        }
-    }
-
-    //Токен я брал из Swagger, авторизовываясь за админа
-    //user@example.com string1, данные для входа админа (закреплены в беседе в ВК)
-    //Токен ручками можно вставить, но живёт он полчаса где-то
-    async function fetchUsers() {
-        const token = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJVc2VyUm9sZSI6IkFkbWluIiwianRpIjoiOTE5YTBjMWEtYjAzMi00ODhkLTg0NzgtYWZhMTdlZjk5OGZjIiwibmJmIjoxNzA5MDYyNzAzLCJleHAiOjE3MDkwNjYzMDMsImlhdCI6MTcwOTA2MjcwMywiaXNzIjoiSXNzdWVyIiwiYXVkIjoiQXVkaWVuY2UifQ.RpuBAqI-2YeoZBgN6PtL04wA9WK23orKgjN660ynPqr78bJpZsnmMmHRztwcjHtllPA3O9GwdBb5CkbIORqnAA';
-
-        try {
-            setLoading(true);
-            const response = await fetch('https://win.jij.li/api/users?Roles=Student', {
-                method: 'GET',
-                headers: {
-                    //Саня говорил, что Bearer писать перед токеном не нужно
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            const users = await response.json();
-            setUsers(users);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            setLoading(false);
-        }
+  useEffect(() => {
+    const token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJVc2VyUm9sZSI6IkFkbWluIiwianRpIjoiNzM0ZmQ3YWYtMWJiYy00MDNhLWI0OTItYTgzZmIwYjk3MDQ1IiwibmJmIjoxNzA5MTQ0MzE4LCJleHAiOjE3MDkxNDc5MTgsImlhdCI6MTcwOTE0NDMxOCwiaXNzIjoiSXNzdWVyIiwiYXVkIjoiQXVkaWVuY2UifQ.E14EmNfg2XNFn4oZf8lDtnXuZg70LyRsmN2Mo3fuPOIqQUvIt3i46Qgj6YxV8qCv6HvIz59XQCUwRx08DUOz4Q"
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://win.jij.li/api/users?Roles=Student&Roles=Teacher&Roles=Principal&Roles=Admin', {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Access-Control-Allow-Origin': '*'
+            }
+        });
+        const jsonData = await response.json();
+        setData(jsonData);
+      } 
+      catch (error) 
+      {
+        console.error('Error fetching data:', error);
+      }
     };
 
-    useEffect(() => {
-        fetchUsers()
-    }, [])
+    fetchData();
+  }, []);
 
-    return (
-        <>
-            <Header type="authorized" page={PageName.USERS}/>
-            <Container className='mt-5'>
-                <UsersFilter/>
-                <Row className='justify-content-center'>
-                    {!loading && (users.map(user => (
-                            <UserCard key={user.id} user={user} />
-                    )))}
-                </Row>
-            </Container>
-        </>
-    );
-}
+  return (
+    <>
+    <Header type="authorized" page={PageName.USERS}/>
+    <Container className='mt-5'>
+        <UsersFilter/>
+        <Row className='justify-content-center'>
+            {users ? (users.value.map(user => (
+                    <UserCard key={user.id} role={user.userRole} email={user.email} name={user.fullName} />
+            ))) : <p>Loading</p>}
+        </Row>
+    </Container>
+</>
+  );
+};
 
-export default Users;
+export default AsyncComponent;
