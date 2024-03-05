02cb98731aa7d200d065abbee7975f8176fb0432
DreamsNPromises <aregoshin9v@gmail.com>
2024-03-05 13:54:37 +0700

    Добавлена создание ключей
    Добавлено:
    - Создание ключа для одной аудитории
    - Создание ключей для диапазона аудиторий

--- src/components/KeysPage/KeysFilter.js
+++ src/components/KeysPage/KeysFilter.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Stack, Row, Col, Form, Dropdown } from 'react-bootstrap';
+import { Stack, Row, Col, Form, Dropdown, Button, ButtonGroup, ToggleButton, Modal } from 'react-bootstrap';
 import useInput from '../../hooks/use-input.js';
 import { token } from '../../const/const-token-temporarily';
 import { post } from '../../methods/apiUtils.js';
@@ -10,15 +10,42 @@ function KeysFilter(props) {
         inPrincipal: ""
     });
 
-    /*const handleAddKey = async () => {
-        try {
-            const newKey = await post('/keys/Create', token, { auditory: values.audience });
-            // Здесь можно выполнить какие-то действия после успешного создания ключа, например, обновление списка ключей
-            console.log('New key created:', newKey);
-        } catch (error) {
-            console.error('Error creating new key:', error);
+    const [showModal, setShowModal] = useState(false);
+    const [multipleAudience, setMultipleAudience] = useState(false);
+
+    const handleAddKey = async () => {
+        if (multipleAudience) {
+            const start = parseInt(values.startAudience);
+            const end = parseInt(values.endAudience);
+
+            if (isNaN(start) || isNaN(end) || start >= end) {
+                console.error('Invalid audience range');
+                return;
+            }
+
+            const newKeys = [];
+
+            for (let i = start; i <= end; i++) {
+                try {
+                    const newKey = await post('/keys/Create', token, { auditory: i.toString() });
+                    newKeys.push(newKey);
+                } catch (error) {
+                    console.error('Error creating new key:', error);
+                }
+            }
+
+            // Обработка новых ключей
+            console.log('New keys created:', newKeys);
+        } else {
+            // Логика для добавления одного ключа
+            try {
+                await post('/keys/Create', token, { auditory: values.audience });
+            } catch (error) {
+                console.error('Error creating new key:', error);
+            }
         }
-    };*/
+        setShowModal(false); // Закрываем модальное окно после добавления ключа
+    };
 
     return (
         <Stack className='darkblue border-radius-small mx-auto' style={{ width: "50%" }}>
@@ -47,25 +74,44 @@ function KeysFilter(props) {
                 <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={3} className='p-6'>
 
                 </Col>
-                <Col xxl={2} xl={3} lg={3} md={4} sm={4} xs={4} className='p-6'>
-                    <Dropdown>
-                        <Dropdown.Toggle
-                            className='time-and-date custom-button-shadow add-circle-button mx-auto'
-                            style={{ borderRadius: "100%" }}
-                            onClick={props.handleAddKey}>
-                            +
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
+                <Col xxl={2} xl={3} lg={3} md={4} sm={4} xs={4} className='p-6' style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                    <Button onClick={() => setShowModal(true)}>+</Button>
+                    <Modal show={showModal} onHide={() => setShowModal(false)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Добавить ключ</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
                             <Form>
-                                <Form.Control type="date" className='radiusnone from' id='from' value={values.from} onChange={handleChange} />
-                                <Form.Control type="date" className='radiusnone to' id='to' value={values.to} onChange={handleChange} />
-                                <Form.Select className='radiusnone' value={values.filter} onChange={handleChange} id='filter'>
-                                    <option value="Desc" className='radiusnone'>По убыванию</option>
-                                    <option value="Asc" className='radiusnone'>По возрастанию</option>
-                                </Form.Select>
+                                <Form.Group>
+                                    <Form.Label>Выберите тип ключа:</Form.Label>
+                                    <Button onClick={() => setMultipleAudience(false)} active={!multipleAudience}>Один</Button>{' '}
+                                    <Button onClick={() => setMultipleAudience(true)} active={multipleAudience}>Несколько</Button>
+                                </Form.Group>
+                                {multipleAudience ? (
+                                    <Form.Group>
+                                        <Form.Label>Диапазон аудиторий:</Form.Label>
+                                        <Row>
+                                            <Col>
+                                                <Form.Control type="text" placeholder="Начало" value={values.startAudience} onChange={handleChange} id='startAudience' />
+                                            </Col>
+                                            <Col>
+                                                <Form.Control type="text" placeholder="Конец" value={values.endAudience} onChange={handleChange} id='endAudience' />
+                                            </Col>
+                                        </Row>
+                                    </Form.Group>
+                                ) : (
+                                    <Form.Group>
+                                        <Form.Label>Номер аудитории:</Form.Label>
+                                        <Form.Control type="text" placeholder="Номер аудитории" value={values.audience} onChange={handleChange} id='audience' />
+                                    </Form.Group>
+                                )}
                             </Form>
-                        </Dropdown.Menu>
-                    </Dropdown>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={() => setShowModal(false)}>Отмена</Button>
+                            <Button variant="primary" onClick={handleAddKey}>Добавить</Button>
+                        </Modal.Footer>
+                    </Modal>
                 </Col>
                 {/*<Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={2} className='p-6'>
                     <Form.Control placeholder="Поиск по имени" className='search verySmallRadius' value={values.searchName} onChange={handleChange} id='searchName' />
