13731ed183547c11666c929839169cf6d77e8a89
YurkinaSonya <yurkina.sonya@ya.ru>
2024-03-05 12:23:12 +0700

    reg layout

--- app/src/main/java/com/example/keybooking/data/dto/UserRegister.kt
+++ app/src/main/java/com/example/keybooking/data/dto/UserRegister.kt
@@ -2,9 +2,8 @@ package com.example.keybooking.data.dto
 
 import com.example.keybooking.data.dto.DTO
 
-data class UserRegister(val userName: String,
-                        val name: String,
-                        val password: String,
-                        val email: String,
-                        val birthDate: String,
-                        val gender: Int) : DTO
+data class UserRegister(
+    val password: String,
+    val fullName: String,
+    val email: String
+) : DTO
