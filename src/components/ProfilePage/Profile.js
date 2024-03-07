ec40f725753ec156e4ac428594dbefd53707fcdc
DreamsNPromises <aregoshin9v@gmail.com>
2024-03-07 11:04:54 +0700

    Улучшен визуал страницы "Профиль"

--- src/components/ProfilePage/Profile.js
+++ src/components/ProfilePage/Profile.js
@@ -1,4 +1,4 @@
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row, Stack } from "react-bootstrap";
 import { PageName } from "../../const/const-pagesnames";
 import Header from "../Header";
 import { useEffect, useState } from "react";
@@ -47,34 +47,45 @@ function Profile(props) {
     return (
         <>
             <Header type="authorized" page={PageName.PROFILE} />
-            <Container className='mt-5 minwidth-540'>
-                <h2>Профиль пользователя</h2>
-                <div>
-                    <p><strong>Email:</strong> {profileData.email}</p>
-                </div>
-                <Form onSubmit={handleFormSubmit}>
-                    <Form.Group controlId='formName'>
-                        <Form.Label>Имя</Form.Label>
-                        <Form.Control
-                            type='text'
-                            placeholder='Введите ваше имя'
-                            value={profileData.fullName}
-                            onChange={(e) => setProfileData({ ...profileData, fullName: e.target.value })}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId='formPassword'>
-                        <Form.Label>Пароль</Form.Label>
-                        <Form.Control
-                            type='password'
-                            placeholder='Введите ваш пароль'
-                            value={userPassword}
-                            onChange={(e) => setUserPassword(e.target.value)}
-                        />
-                    </Form.Group>
-                    <Button variant='primary' type='submit'>
-                        Сохранить изменения
-                    </Button>
-                </Form>
+            <Container className='mt-5 minwidth-540 mx-auto'>
+                <Row className='justify-content-center'>
+                    <Col xs={12} md={8} lg={6}>
+                        <Stack className='border-radius-small darkAndLight'>
+                            <Row className='mt-3 mx-1 mb-2'>
+                                <h4>Профиль пользователя</h4>
+                            </Row>
+                        </Stack>
+                        <Stack className="border-darkblue">
+                            <div className="mt-3 mx-3">
+                                <p>Имя: {profileData.fullName}</p>
+                                <p>Email: {profileData.email}</p>
+                            </div>
+                            <Form onSubmit={handleFormSubmit}>
+                                <Form.Group controlId='formName' className="mb-3 mx-3">
+                                    <Form.Label>Имя</Form.Label>
+                                    <Form.Control
+                                        type='text'
+                                        placeholder='Введите ваше имя'
+                                        value={profileData.fullName}
+                                        onChange={(e) => setProfileData({ ...profileData, fullName: e.target.value })}
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId='formPassword' className="mb-3 mx-3">
+                                    <Form.Label>Пароль</Form.Label>
+                                    <Form.Control
+                                        type='password'
+                                        placeholder='Введите ваш пароль'
+                                        value={userPassword}
+                                        onChange={(e) => setUserPassword(e.target.value)}
+                                    />
+                                </Form.Group>
+                                <Button variant='primary' type='submit' className="mb-3 mx-3 custom-button">
+                                    Сохранить изменения
+                                </Button>
+                            </Form>
+                        </Stack>
+                    </Col>
+                </Row>
             </Container>
         </>
     );
