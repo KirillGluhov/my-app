c3e57e00eecd7621a3ec9baf0364e92e16649f1c
Kirill Glukhov <valeralunin34@gmail.com>
2024-03-09 22:15:08 +0700

    Адаптивность ключей

--- src/components/KeysPage/KeyCard.js
+++ src/components/KeysPage/KeyCard.js
@@ -17,21 +17,19 @@ function KeyCard({keyData}) {
     };
 
     return (
-        <Stack className='border-darkblue mx-auto' style={{ width: '50%', padding: '0px' }}>
-            <Row className='mt-3 mx-1 '>
-                <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={4} className='mb-3 p-6 border-radius-small-all center'>
-                <Form.Control placeholder="-" className='radiusnone center' plaintext readOnly defaultValue={keyData.auditory} />
+        <Stack className='border-darkblue mx-auto second-stack' style={{ padding: '0px' }}>
+            <Row className='mt-3 mx-1'>
+                <Col xs={5} sm={5} md={4} lg={4} xl={5} xxl={4} className='mb-3 p-6 border-radius-small-all center'>
+                    <Form.Control placeholder="-" className='radiusnone center' plaintext readOnly defaultValue={keyData.auditory} />
                 </Col>
-                <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={3} className='mb-3 p-6'>
+                <Col xs={4} sm={4} md={3} lg={3} xl={3} xxl={3} className='mb-3 p-6'>
                     <Form.Control placeholder="-" className='radiusnone center' plaintext readOnly defaultValue={keyData.isInPrincipalOffice ? "В деканате" : "На руках"} />
                 </Col>
-                <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={3} className='mb-3 p-6'>
-                    <Form.Control placeholder="Вернуть" className='radiusnone center' plaintext readOnly />
+                <Col xs={3} sm={3} md={3} lg={3} xl={2} xxl={3} className='mb-3 p-6 d-flex justify-content-center'>
+                    <Button variant="primary" className='center back-to-principal-button'>Вернуть</Button>
                 </Col>
-                <Col xxl={2} xl={2} lg={2} md={2} sm={2} xs={2} className='mb-3 p-0'>
-                    <Box style={{ display: 'flex', justifyContent: 'space-evenly' }}>
-                        <Button className='border-red border-2 rounded-0 d-flex' variant='secondary' style={{ width: '38px', background:"#F97D7D", borderColor: "#823333"}} onClick={handleNoButtonClick}><No/></Button>
-                    </Box>
+                <Col xs={12} sm={12} md={2} lg={2} xl={2} xxl={2} className='mb-3 p-0 d-flex justify-content-center'>
+                    <Button className='border-red border-2 rounded-0 d-flex' variant='secondary' style={{ width: '38px', background: "#F97D7D", borderColor: "#823333" }} onClick={handleDeleteKey}><No /></Button>
                 </Col>
             </Row>
         </Stack>
